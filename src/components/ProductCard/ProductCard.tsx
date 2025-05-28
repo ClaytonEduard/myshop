bf5e76944b69777b8532380c62eba1f004e994ac
Clayton Eduard <clayton_eduard@hotmail.com>
2025-05-28 17:06:23 -0300

    ProductCard Finalizado

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,16 +1,31 @@
 import { FiShoppingCart } from "react-icons/fi";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
 import * as S from "./styles";
-export const ProductCard: React.FC = () => {
+import { Product } from "../../data/products";
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   return (
     <S.Card>
-      <S.ProductImage
-        src="https://fakestoreapi.com/img/1692255251854-xbox.jpg"
-        alt="Product"
-      />
-      <S.ProductTitle>Product Title</S.ProductTitle>
+      <S.ProductImage src={product.image} alt={product.description} />
+      <S.ProductTitle>{product.title}</S.ProductTitle>
       <S.ReviewPriceContainer>
-        <S.Review>⭐⭐⭐⭐⭐</S.Review>
-        <S.Price>$99.99</S.Price>
+        <S.Review>
+          <AiFillStar />
+          {Array.from({ length: 5 }, (_, index) =>
+            index < Math.round(product.rating[0]) ? (
+              <AiFillStar key={index} />
+            ) : (
+              <AiOutlineStar key={index} />
+            )
+          )}
+          ( {` ${product.rating[0]}`})
+        </S.Review>
+        <S.Price>R$ {product.price}</S.Price>
       </S.ReviewPriceContainer>
       <S.AddToCartButtonWrapper>
         <S.AddToCartButton>
--- src/components/ProductCard/styles.ts
+++ src/components/ProductCard/styles.ts
@@ -3,26 +3,53 @@ import styled from "styled-components";
 export const Card = styled.article`
   background-color: #fff;
   width: 100%;
+  padding: 2rem;
   margin: 0 auto;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  border-radius: 10px;
+  justify-content: space-between;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 export const ProductImage = styled.img`
-  width: 90%;
-  height: 200px;
+  width: 250px;
+  height: 400px;
+
+  object-fit: contain; // contai a imgem dentro do card sem distorcert
 `;
 export const ProductTitle = styled.h2`
   font-weight: 500;
   font-size: 1.2rem;
+
+  min-height: 3rem;
+
+  margin-top: 1rem;
 `;
 export const ReviewPriceContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
+
+  margin-top: 1rem;
 `;
-export const Review = styled.span``;
+export const Review = styled.span`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+
+  font-size: 0.9rem;
+  color: #666;
+  svg {
+    color: #f39c12; // cor do icone de estrela
+  }
+`;
 export const Price = styled.strong``;
 export const AddToCartButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
+  width: 100%;
   align-items: center;
   margin-top: 1rem;
 `;
@@ -30,15 +57,17 @@ export const AddToCartButton = styled.button`
   background-color: rgb(86, 44, 252);
   border: none;
   border-radius: 5px;
-  height: 30px;
-  padding: 0 1rem;
+  height: 40px;
   cursor: pointer;
   font-size: 0.8rem;
   color: #fff;
-
+  width: 100%;
   display: flex;
   align-items: center;
   gap: 0.3rem;
+
+  justify-content: center;
+
   svg {
     font-size: 0.9rem;
   }
