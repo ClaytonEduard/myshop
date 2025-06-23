385af620e579685f413a5b84d70f360a00e5545f
Clayton Eduard <clayton_eduard@hotmail.com>
2025-06-23 10:58:06 -0300

    Configurando uma estilização basica

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -12,4 +12,12 @@ export const GlobalStyles = createGlobalStyle`
         body, button{
         font-family: 'Poppins', sans-serif;
         }
+        button {
+            cursor: pointer;
+            transition: filter 0.2s;
+            
+            &:hover {
+                filter: brightness(1.2);
+            }
+        }
 `;
